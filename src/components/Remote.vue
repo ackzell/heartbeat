32652a38f29a74fc2d55d87713a3265d99f3f72c
ackzell <ackzell@gmail.com>
2017-06-23 22:39:10 -0500

    Making use of vuetify components for the remotes list as well as remote items.

--- src/components/Remote.vue
+++ src/components/Remote.vue
@@ -1,5 +1,36 @@
 <template>
-  <div>
-    This is a remote!
-  </div>
+    <v-list-item>
+      <v-list-tile>
+
+        <v-list-tile-content>
+          <v-list-tile-title>{{ name }}</v-list-tile-title>
+        </v-list-tile-content>
+
+        <v-list-tile-action>
+          <i v-if="status === 'UP'"
+              class="material-icons md-18 online">swap_vertical_circle</i>
+          <i v-else
+              class="material-icons md-18 offline">report_problem</i>
+        </v-list-tile-action>
+
+      </v-list-tile>
+    </v-list-item>
 </template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    status: String
+  }
+}
+</script>
+
+<style lang="stylus">
+.offline
+  color: #8091ff
+
+.online
+  color: #36e0d2
+
+</style>
--- src/components/RemotesList.vue
+++ src/components/RemotesList.vue
@@ -1,14 +1,44 @@
 <template>
-  <div>
-    <remote v-for="x in 10"></remote>
-  </div>
+  <v-list>
+    <remote v-for="(key, val, idx) in myRemotes" :key="idx" :name="key.alias"></remote>
+  </v-list>
 </template>
 
 <script>
 import Remote from './Remote.vue'
+
+
+const services = [
+  {
+    uri: 'http://localhost:8761/health',
+    alias: 'Registry',
+  },
+  {
+    uri: 'http://localhost:9999/uaa/health',
+    alias: 'SSO',
+  },
+  {
+    uri: 'http://localhost:8765/health',
+    alias: 'Gateway this is a very long name 2234',
+  },
+  {
+    uri: 'http://localhost:9090/health',
+    alias: 'Homepage',
+  },
+  {
+    uri: 'http://localhost:8010/health',
+    alias: 'Consumer Exposure longer name',
+  }
+];
+
 export default {
   components: {
     Remote
+  },
+  data() {
+    return {
+      myRemotes: services
+    }
   }
 }
 </script>
