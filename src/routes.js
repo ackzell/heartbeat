e235c8d8c3dadc9133556831e6a832bd4f42e599
ackzell <ackzell@gmail.com>
2017-10-09 08:33:58 -0500

    adding a build task, also a specialized package.json for builds

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import RemotesList from './components/RemotesList.vue'
 import RemoteForm from './components/RemoteForm.vue'
 
 export const routes = [
+  { path: '*', redirect: '/' },
   { path: '', component: RemotesList },
   { path: '/remote', component: RemoteForm },
   {
